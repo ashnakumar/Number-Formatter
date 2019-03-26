9bdcb3c165bde6ff89ab6eef808cb23a34490e5f
ashnakumar <35329978+ashnakumar@users.noreply.github.com>
2019-03-26 18:40:06 +0530

    format large numbers into human readable format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgNumberFormatterModule } from './modules/ng-number-formatter/ng-number-formatter.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgNumberFormatterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/ng-number-formatter/ng-number-formatter.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgNumberFormatterComponent } from './ng-number-formatter.component';
+
+@NgModule({
+  declarations: [NgNumberFormatterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NgNumberFormatterModule { }
